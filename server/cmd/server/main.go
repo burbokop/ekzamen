0b5ac473182b492538fd17ea7fdf2592124f197f
burbokop <burbokop@gmail.com>
2021-01-12 10:07:03 +0200

    fgfg

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"log"
+	"os"
+
+	"github.com/burbokop/ekzamen/server/tree"
 )
 
 func main() {
+	binTree := &tree.BinaryTree{}
+	binTree.insert(100.0)
+
+	print(os.Stdout, binTree.root, 0, 'M')
+
 	// Create the server.
-	if err := StartServer(); err == nil {
-		log.Println("Starting chat server...")
-	} else {
-		log.Fatalf("Cannot initialize banking server: %s", err)
-	}
+	//if err := StartServer(); err == nil {
+	//	log.Println("Starting chat server...")
+	//} else {
+	//	log.Fatalf("Cannot initialize banking server: %s", err)
+	//}
 }
